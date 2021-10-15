67312053c0a5f6b1453b35d0c5555fcfa75389b2
Morgan Hallgren <morganh@axis.com>
2021-10-15 15:04:34 +0200

    new events and more tests

--- device/device.go
+++ device/device.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hallgren/eventsourcing"
 )
 
+// Device entity
 type Device struct {
 	eventsourcing.AggregateRoot
 	Serial     string
@@ -14,35 +15,68 @@ type Device struct {
 	IP         string
 }
 
+// Device Events
+
+// DiscoveredViaBonjour constructor event
 type DiscoveredViaBonjour struct {
 	IP     string
 	Serial string
 }
 
+// AddedToSite when the device is part of the site
 type AddedToSite struct{}
 
-var ErrAlreadyPartOfSite = fmt.Errorf("device is already part of site")
+// RemovedFromSite when the device is no longer part of the site
+type RemovedFromSite struct{}
 
+// Disconnected when the device is offline
+type Disconnected struct{}
+
+// Connected when the device is online
+type Connected struct{}
+
+// Errors returned on failing commands
+var ErrAlreadyPartOfSite = fmt.Errorf("device is already part of site")
+var ErrNotPartOfSite = fmt.Errorf("device is not part of site")
+
+// Transitions method to build the current state of the device
 func (d *Device) Transition(event eventsourcing.Event) {
 	switch e := event.Data.(type) {
 	case *DiscoveredViaBonjour:
 		d.IP = e.IP
 		d.Serial = e.Serial
+	case *Connected:
+		d.Connected = true
 	case *AddedToSite:
 		d.PartOfSite = true
+	case *RemovedFromSite:
+		d.PartOfSite = false
 	}
 }
+
+// Constructor
 
 func FoundViaBonjour(ip, serial string) *Device {
 	d := Device{}
 	d.TrackChange(&d, &DiscoveredViaBonjour{IP: ip, Serial: serial})
+	d.TrackChange(&d, &Connected{})
 	return &d
 }
+
+// Commands
 
 func (d *Device) AddToSite() error {
 	if d.PartOfSite {
 		return ErrAlreadyPartOfSite
 	}
 	d.TrackChange(d, &AddedToSite{})
+	return nil
+}
+
+func (d *Device) RemoveFromSite() error {
+	if !d.PartOfSite {
+		return ErrNotPartOfSite
+	}
+	d.TrackChange(d, &RemovedFromSite{})
 	return nil
 }
--- device/device_test.go
+++ device/device_test.go
@@ -9,8 +9,11 @@ import (
 
 func TestDiscoverViaBonjour(t *testing.T) {
 	d := device.FoundViaBonjour("192.168.0.8", "AAABBB123")
-	if len(d.Events()) != 1 {
-		t.Fatalf("expected 1 event got %d", len(d.Events()))
+	if len(d.Events()) != 2 {
+		t.Fatalf("expected 2 event got %d", len(d.Events()))
+	}
+	if !d.Connected {
+		t.Fatal("device should be connected")
 	}
 	spew.Dump(d.Events())
 }
@@ -21,16 +24,46 @@ func TestAddToSite(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(d.Events()) != 2 {
-		t.Fatalf("expected 2 event got %d", len(d.Events()))
+	if len(d.Events()) != 3 {
+		t.Fatalf("expected 3 event got %d", len(d.Events()))
 	}
-	if d.Events()[1].Reason != "AddedToSite" {
-		t.Fatalf("2 event is not AddedToSite was %s", d.Events()[1].Reason)
+	if d.Events()[2].Reason != "AddedToSite" {
+		t.Fatalf("last event is not AddedToSite was %s", d.Events()[1].Reason)
 	}
 	spew.Dump(d.Events())
 
 	err = d.AddToSite()
 	if err == nil {
 		t.Fatal("should not add device to site twice")
+	}
+}
+
+func TestRemoveFromSite(t *testing.T) {
+	d := device.FoundViaBonjour("192.168.0.8", "AAABBB123")
+	d.AddToSite()
+
+	err := d.RemoveFromSite()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(d.Events()) != 4 {
+		t.Fatalf("expected 4 event got %d", len(d.Events()))
+	}
+
+	if d.Events()[3].Reason != "RemovedFromSite" {
+		t.Fatalf("last event should be RemovedFromSite but was: %s", d.Events()[2].Reason)
+	}
+
+	if d.PartOfSite {
+		t.Fatal("the device should not be part of site")
+	}
+}
+
+func TestDisconnect(t *testing.T) {
+	d := device.FoundViaBonjour("192.168.0.8", "AAABBB123")
+	//d.NoLongerReachable()
+
+	if len(d.Events()) != 2 {
+		t.Fatalf("expected 2 event got %d", len(d.Events()))
 	}
 }
