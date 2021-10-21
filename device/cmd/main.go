a9ab1c09bb78c67afcbbf7002ec37c771a5192ae
Morgan Hallgren <morganh@axis.com>
2021-10-21 15:23:41 +0200

    add more examples

--- device/cmd/main.go
+++ device/cmd/main.go
@@ -30,24 +30,54 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = sqlEventStore.Migrate()
-	if err != nil {
-		//panic(err)
-		fmt.Println(err)
-	}
+	sqlEventStore.Migrate()
 
-	// Create a repository to handle event sourced
+	// create a entity and some events
 	d := device.FoundViaBonjour("192.168.0.99", "AABBCC")
 	d.AddToSite()
 
 	spew.Dump(d)
 
-	//repository := eventsourcing.NewRepository(sqlEventStore, nil)
-	//repository.Save(d)
+	// repository
+	//
+	repository := eventsourcing.NewRepository(sqlEventStore, nil)
+	repository.Save(d)
+	fmt.Println("########")
+	spew.Dump(d)
 
-	//fmt.Println("########")
-	//d2 := device.Device{}
-	//repository.Get(d.ID(), &d2)
-	//spew.Dump(d2)
+	// event subscription
+	//
+	// all events
+	subAll := repository.SubscriberAll(func(e eventsourcing.Event) {
+		fmt.Println("all", e)
+	})
+	subAll.Subscribe()
 
+	// specific event
+	subReason := repository.SubscriberSpecificEvent(func(e eventsourcing.Event) {
+		fmt.Println("specific event", e.Reason, e)
+	}, &device.RemovedFromSite{})
+	subReason.Subscribe()
+
+	// concurrency garanty
+	//
+	d2 := device.Device{}
+	repository.Get(d.ID(), &d2)
+
+	d2.RemoveFromSite()
+	err = repository.Save(&d2)
+	if err != nil {
+		fmt.Println("could not store events on d2", err)
+	}
+
+	d.RemoveFromSite()
+	err = repository.Save(d)
+	if err != nil {
+		fmt.Println("could not store events on d", err)
+	}
+
+	// global events
+	//
+	globalEvents, _ := sqlEventStore.GlobalEvents(1, 100)
+	spew.Dump(globalEvents)
 }
